8744642f7c6fb7eab2f2d7b76e7ec482cec1ffe1
FrancescoSica16 <francesco.sica16@gmail.com>
2021-08-05 15:32:10 +0200

    var definition

---
+++ myscript.js
@@ -0,0 +1,14 @@
+
+const oneKmPrice = 0.21;
+const under18Sales = 0.2;
+const over65Sales = 0.4;
+
+
+let numKm = parseInt ( prompt("Quanti Km vuoi percorrere?") );
+let agePass = parseInt ( prompt("Quanti anni hai?") );
+
+var priceKm = numKm * oneKmPrice ; 
+
+
+
+
